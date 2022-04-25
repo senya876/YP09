4332931f2f67540a2e6676ad67d5a815575352ff
Senya Work <senyaWOT@yandex.ru>
2022-04-25 10:05:20 +0300

    Modal-3

--- js/modal.js
+++ js/modal.js
@@ -1,20 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     var modalButtons = document.querySelectorAll('.call-btn--openmodal'),
-        overlay = document.querySelector('.modal-overlay'),
         closeButtons = document.querySelectorAll('.popup-close'),
         el = document.querySelector('.popup-call');
  
     modalButtons.forEach(function(item){
         item.addEventListener('click', function(e) {
-
             e.preventDefault();
-
             el.style.display='block';
-            overlay.classList.add('active');
        }); 
     }); 
  
-    let a = closeButtons.forEach(function(item){
+    closeButtons.forEach(function(item){
         item.addEventListener('click', function() {
             el.style.display='none';
         });
@@ -22,6 +18,56 @@ document.addEventListener('DOMContentLoaded', function() {
 
     el.addEventListener('click',(e)=>{
         if(e.target.classList.contains('popup-call') || e.target.classList.contains('popup-close')){
+            el.style.display='none';
+        }
+    })
+}); 
+
+document.addEventListener('DOMContentLoaded', function()  {
+    var modalButtons = document.querySelectorAll('.sentence-btn'),
+        closeButtons = document.querySelectorAll('.popup-close'),
+        el = document.querySelector('.popup-discount');
+ 
+    modalButtons.forEach(function(item){
+        item.addEventListener('click', function(e) {
+            e.preventDefault();
+            el.style.display='block';   
+       }); 
+    }); 
+ 
+    closeButtons.forEach(function(item){
+        item.addEventListener('click', function() {
+            el.style.display='none';
+        });
+    }); 
+
+    el.addEventListener('click',(e)=>{
+        if(e.target.classList.contains('popup-discount') || e.target.classList.contains('popup-close')){
+            el.style.display='none';
+        }
+    })
+}); 
+
+document.addEventListener('DOMContentLoaded', function()  {
+    var modalButtons = document.querySelectorAll('.check-btn'),
+        closeButtons = document.querySelectorAll('.popup-close'),
+        el = document.querySelector('.popup-discount');
+ 
+    modalButtons.forEach(function(item){
+        item.addEventListener('click', function(e) {
+            e.preventDefault();
+            el.style.display='block';   
+       }); 
+    }); 
+ 
+    closeButtons.forEach(function(item){
+        item.addEventListener('click', function() {
+            el.style.display='none';
+        });
+    }); 
+
+    el.addEventListener('click',(e)=>{
+        if(e.target.classList.contains('popup-discount') || e.target.classList.contains('popup-close')){
             el.style.display='none';
         }
     })
